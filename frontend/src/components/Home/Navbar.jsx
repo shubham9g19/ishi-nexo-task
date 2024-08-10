576eb3670a4dc1d3fcfdbf01982acc087e58d91f
shubham9g19 <91056346+shubham9g19@users.noreply.github.com>
2024-08-10 14:19:33 +0530

    Frontend Completed

--- frontend/src/components/Home/Navbar.jsx
+++ frontend/src/components/Home/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { PiLinktreeLogoBold } from "react-icons/pi";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-function Navbar() {
+function Navbar({ remove_header }) {
   const { token, setToken } = useAuth();
   const navigate = useNavigate();
   const handleLogout = () => {
@@ -15,71 +15,77 @@ function Navbar() {
         <div className="flex items-center h-16">
           <div className="flex items-center">
             <div>
-              <PiLinktreeLogoBold
-                style={{ fontSize: "40px" }}
-                className="mt-4"
-              />
+              <Link to="/">
+                <PiLinktreeLogoBold
+                  style={{ fontSize: "40px" }}
+                  className="mt-4"
+                />
+              </Link>
             </div>
           </div>
-          <div className="flex items-center mt-6 ml-4 text-[1.12rem] font-sans font-semibold">
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)] hover:bg-opacity-90"
-            >
-              Templates
-            </a>
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
-            >
-              Marketplace
-            </a>
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
-            >
-              Discover
-            </a>
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
-            >
-              Pricing
-            </a>
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
-            >
-              Learn
-            </a>
-            <a
-              href="/"
-              className="text-[rgb(103,107,95)] inline-flex px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="size-5 mt-1 mx-1"
+          {remove_header ? null : (
+            <div className="flex items-center mt-6 ml-4 text-[1.12rem] font-sans font-semibold">
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)] hover:bg-opacity-90"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Search
-            </a>
-          </div>
+                Templates
+              </a>
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
+              >
+                Marketplace
+              </a>
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
+              >
+                Discover
+              </a>
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
+              >
+                Pricing
+              </a>
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
+              >
+                Learn
+              </a>
+              <a
+                href="/"
+                className="text-[rgb(103,107,95)] inline-flex px-3 py-2 rounded-lg hover:bg-[rgb(239,240,236)]"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="size-5 mt-1 mx-1"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Search
+              </a>
+            </div>
+          )}
         </div>
         <div className="inline-flex items-center ml-20 text-base font-semibold ">
           {token ? (
             <>
-              <Link to="/profile">
-                <button className="border rounded-md bg-[rgb(239,240,236)] w-24 h-16 mx-2 mt-3 ">
-                  Profile
-                </button>
-              </Link>
+              {remove_header ? null : (
+                <Link to="/profile">
+                  <button className="border rounded-md bg-[rgb(239,240,236)] w-24 h-16 mx-2 mt-3 ">
+                    Profile
+                  </button>
+                </Link>
+              )}
               <button
                 onClick={handleLogout}
                 className="text-[1.15rem] text-white border rounded-full bg-[rgb(30,35,48)] w-40 h-16 mt-3"
--- frontend/src/pages/LinkTree.jsx
+++ frontend/src/pages/LinkTree.jsx
@@ -1,9 +1,129 @@
 import Navbar from "../components/Home/Navbar";
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { api } from "../services/api";
 function LinkTreePage() {
+  const [links, setLinks] = useState([]);
+  const [profile, setProfile] = useState({});
+  const { username } = useParams();
+  useEffect(() => {
+
+    const fetchProfileLinks = async () => {
+      const response = await api.get(`/links/${username}/public`);
+      setLinks(response.data.links);
+      setProfile({
+        username: response.data.username,
+        bio: response.data.bio,
+        profile_pic: response.data.profile_pic,
+      });
+    };
+    fetchProfileLinks();
+  }, []);
   return (
     <>
-      <Navbar />
-      <div></div>
+      <Navbar remove_header={true} />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="sc-bdfBwQ sc-gsTCUz sc-aemoO iIoWHx bhdLno iOmLqx">
+        <div className="sc-bdfBwQ sc-gsTCUz ewSEqQ bhdLno">
+          <div className="sc-bdfBwQ eqtAab">
+            <div className="sc-bdfBwQ sc-fbkhIv iCHEqi erRfmt"></div>
+            {profile.username ? (
+              <div className="flex flex-col items-center">
+                {profile.profile_pic ? (
+                  <div className="mb-4" id="profile-picture">
+                    <img
+                      className="rounded-full object-contain"
+                      src="https://ugc.production.linktr.ee/6031db72-f0c0-4d17-915f-82c370237305_IMG-8611-Original-2.jpeg?io=true&amp;size=avatar-v3_0"
+                      alt=""
+                      style={{ width: 96, height: 96 }}
+                      role="presentation"
+                      data-testid="ProfileImage"
+                      crossorigin="anonymous"
+                    />
+                  </div>
+                ) : (
+                  <></>
+                )}
+
+                <div
+                  className="flex items-center mx-3 max-w-full"
+                  id="profile-title"
+                >
+                  <h1
+                    className="text-ellipsis text-lg leading-[1.5] font-[700] text-bodyText"
+                    id="profile-sarahwinter-17"
+                  >
+                    @{profile.username}
+                  </h1>
+                </div>
+                <div className="px-6 mt-[2px]" id="profile-description">
+                  <h2 className="text-center text-profileDescription text-profileDescriptionText leading-profileDescription font-profileDescription text-balance">
+                    {profile.bio}
+                  </h2>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+
+            <br />
+            {links.length === 0 ? (
+              <div>No links found</div>
+            ) : (
+              <div className="mt-xl" id="links-container">
+                {links.map((link) => (
+                  <div key={link._id}>
+                    <div data-id="304232387" className="sc-bdfBwQ pkAuV">
+                      <div
+                        id="304232387"
+                        className="sc-bdfBwQ sc-kLgntA jAmcnV iHaOcn"
+                      ></div>
+                      <div
+                        data-testid="StyledContainer"
+                        className="sc-bdfBwQ sc-dmlrTW eua-dhZ NkwoY group"
+                        type="classNameIC"
+                      >
+                        <a
+                          href={link.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          data-testid="LinkButton"
+                          className="sc-pFZIQ sc-hHftDr ldGKnQ hxNDJq group"
+                          aria-describedby="profile-sarahwinter-17 "
+                          height="auto"
+                        >
+                          <div className="w-full h-full">
+                            <div
+                              data-testid="LinkThumbnail"
+                              className="sc-bdfBwQ sc-gsTCUz sc-bkzZxe bAqGfC bhdLno CvSZl"
+                            >
+                              <img
+                                data-testid="LinkThumbnailImage"
+                                src={link.image}
+                                alt=""
+                                filter=""
+                                loading="lazy"
+                                crossorigin=""
+                                className="sc-iBPRYJ sc-idOhPF gLxBPj ioILqe"
+                              />
+                            </div>
+                            <p className="sc-hKgILt sc-cBNfnY gXKGT inUMaH">
+                              {link.description}
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
